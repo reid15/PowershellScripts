0cfffa7e032c77ae4ebf5cc5270fdf7668971b89
Rob Reid <15robreid@gmail.com>
2018-06-23 13:41:31 -0400

    Added Command Line BAT files

---
+++ CommandLineBATs/BuildDotNetSolution.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" SalesAndDistribution\Zuora\ZuoraAPI.sln
+
+pause
---
+++ CommandLineBATs/CopySqlTemplates.bat
@@ -0,0 +1,3 @@
+xcopy "C:\Users\rreid\AppData\Roaming\Microsoft\SQL Server Management Studio\13.0\Templates\Sql\A_Templates\*" "C:\Users\rreid\Google Drive\Backup\A_Templates" /i /y
+
+pause
